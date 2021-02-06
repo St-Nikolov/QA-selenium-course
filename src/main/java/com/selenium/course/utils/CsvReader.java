a7c4ea036302a47216d3379c726c3e3214326d08
St-nikolov <satanislaw@yahoo.com>
2021-02-06 18:50:55 +0200

    Course Project

--- src/main/java/com/selenium/course/utils/CsvReader.java
+++ src/main/java/com/selenium/course/utils/CsvReader.java
@@ -3,18 +3,21 @@ package com.selenium.course.utils;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
 
-import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class CsvReader {
     public static Object[][] readCsvFile(String fileName) throws IOException, CsvException {
-        CSVReader csvReader = new CSVReader(new FileReader(fileName));
+        CSVReader csvReader = new CSVReader(new InputStreamReader(new FileInputStream(fileName), StandardCharsets.UTF_8));
         List<String[]> csvData = csvReader.readAll();
         Object[][] csvDataObject = new Object[csvData.size()][csvData.get(0).length];
         for(int i = 0; i < csvData.size(); i++) {
             csvDataObject[i] = csvData.get(i);
         }
+        csvReader.close();
         return csvDataObject;
     }
 }
